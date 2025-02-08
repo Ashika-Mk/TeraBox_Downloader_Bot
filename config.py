c55d84aaebb52c5f6da5a70c9d6c6f934083f000
rohit8708913 <koyeb67user@gmail.com>
2025-02-08 07:06:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7781860560:AAFfiklxdn1b0xTq8WUUAsgduLw3YyVfQQE")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7781860560:AAFelYRBvSVXuDDbeAUFplZlVv1qlWYBgbg")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "9698652"))
@@ -24,7 +24,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001"))
 
 #Port
-PORT = os.environ.get("PORT", "5322")
+PORT = os.environ.get("PORT", "5323")
 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://obito:umaid2008@cluster0.engyc.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "test")
