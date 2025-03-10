fe96dffe2b19dd7cc3248ba83451edb9f46115c3
rohit870891 <koyeb77user@gmail.com>
2025-03-10 22:22:56 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -41,7 +41,7 @@ async def check_admin(filter, client, update):
         return False
 
 # Check user subscription in Channels in a more optimized way
-async def is_subscribed(filter, client):
+async def is_subscribed(user_id, filter, client):
     Channel_ids = await db.get_all_channels() or []
 
     if not Channel_ids:
