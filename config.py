48167e26cb92440e460bc1fe42d5c0d679abd6a6
rohit870891 <koyeb77user@gmail.com>
2025-04-25 10:01:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -73,7 +73,7 @@ TUT_VID = os.environ.get("TUT_VID", "https://t.me/delight_link/2")
 
 
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "100"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 
 
 
