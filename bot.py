e48a1a3648eebc0559887b7ce6a6cd4c61434f2c
rohit870891 <koyeb77user@gmail.com>
2025-05-06 22:23:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,3 +86,6 @@ class Bot(Client):
             self.LOGGER(__name__).info("Shutting down...")
         finally:
             loop.run_until_complete(self.stop())
+
+if __name__ == "__main__":
+    Bot().run()
