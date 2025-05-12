1debe4a17311cdaa192f1a9f92cc287df0d05f05
rohit870891 <koyeb77user@gmail.com>
2025-05-13 03:09:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def home():
     return "Bot is running"
 
 def run_flask():
-    flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3413)))
+    flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3414)))
 
 def keep_alive():
     t = Thread(target=run_flask)
