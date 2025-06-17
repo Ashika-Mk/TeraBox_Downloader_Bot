4133e08c82267b6804fe09537b1c1ef85e4e6ad3
rohit870891 <koyeb77user@gmail.com>
2025-06-17 17:16:10 +0530

    Update video.py

--- video.py
+++ video.py
@@ -47,7 +47,7 @@ from collections import defaultdict
 
 #rohit95 old token
 
-TERABOX_API_URL = "https://terabox.web.id"
+TERABOX_API_URL = "https://terabox.delphadex.com"
 TERABOX_API_TOKEN = "85ebfdd8-77d5-4725-a3b6-3a03ba188a5c_7328629001"
 THUMBNAIL = "https://envs.sh/S-T.jpg"
 
